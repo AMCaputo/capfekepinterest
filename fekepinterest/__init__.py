6da5ecf599f3a580c3096dbf3e52448768f892e1
Adelino Caputo <adelinocaputo@gmail.com>
2026-01-16 16:24:57 +0100

    outros ajustes

--- fekepinterest/__init__.py
+++ fekepinterest/__init__.py
@@ -4,7 +4,8 @@ from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///capfekepinterest.db"
+#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///capfekepinterest.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "https://capfekepinterest.onrender.com.db"
 app.config["SECRET_KEY"] = "649be41474027686d97754a466a24bead818b92b2030b4d9"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from fekepinterest import app, database
+from flask import Flask
+from flask_cors import CORS
 
 
-if __name__ == '__main__':
-   database, app.run(debug=True)
+
+""" if __name__ == '__main__':
+   database, app.run(debug=True) """
+CORS(app) # Isso libera o acesso para o seu frontend
+
+app = Flask(__name__)
+CORS(app) # Isso libera o acesso para o seu frontend
