c732da950eae64b4e3e032d27d0ae79d2778fdcb
Adelino Caputo <adelinocaputo@gmail.com>
2026-02-06 15:55:33 +0100

    Atualizacao do siatema

--- fekepinterest/__init__.py
+++ fekepinterest/__init__.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 #app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///capfekepinterest.db"
-app.config["SQLALCHEMY_DATABASE_URI"] = "https://capfekepinterest.onrender.com.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///capfekepinterest.db"
 app.config["SECRET_KEY"] = "649be41474027686d97754a466a24bead818b92b2030b4d9"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
--- main.py
+++ main.py
@@ -1,28 +1,6 @@
-import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-
-# 1. Pega a URL da variável de ambiente que configuramos no Render
-# Se não houver variável (localmente), ele usa um sqlite para não dar erro
-database_url = os.environ.get("DATABASE_URL")
-
-if database_url:
-    # 2. CORREÇÃO CRÍTICA: O Render dá "postgres://", o SQLAlchemy exige "postgresql://"
-    if database_url.startswith("postgres://"):
-        database_url = database_url.replace("postgres://", "postgresql://", 1)
-    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
-else:
-    # Fallback para desenvolvimento local
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-
-db = SQLAlchemy(app)
-
-with app.app_context():
-    db.create_all()  # Isso cria as tabelas automaticamente se elas não existirem
+from fekepinterest import app
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+  
+    app.run(debug=True)
